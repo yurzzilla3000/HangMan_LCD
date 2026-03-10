be9255b9711f9804a72a117ac08cfa7a02759c41
yurzzilla <bu277188@gmail.com>
2026-03-10 18:23:54 +0200

    Working_Known_Letters_Print

--- HangMan_LCD.ino
+++ HangMan_LCD.ino
@@ -2,20 +2,25 @@
 #define PIN_NEXT 4
 #define PIN_PREV 2
 #define PIN_SEL 3
-bool screen = 1;
-byte selected = 0;
+bool screen = 0;
+byte selected = 14;
 LiquidCrystal_I2C lcd(0x27,16,2);
 char letters[26] = "abcdefghijklmnopqrstuvwxyz";
 const char* words[] = {"hello\0","cool\0", "frog\0", "idk\0"};
-// char PlayWord[32] = words[random(0,3)];
-void printWord();
-  // for(int i = 0; i < 31; i++){
-  //   if(PlayWord[i] == "\0"){
-  //     break;
-  //   }
-  //   lcd.print(PlayWord[i]);
-  //   if(i == 15) lcd.setCursor(0,1);
-  // }
+const char* PlayWord = words[random(0,3)];
+char letters_guessed[3] = "oca";
+void printWord(){
+  lcd.clear();
+  for(int i = 0; i < 31; i++){
+    if(PlayWord[i] == '\0'){
+      break;
+    }
+    for(int j = 0; j < 26; j++){
+      if(letters_guessed[j] == PlayWord[i]) lcd.print(PlayWord[i]);
+    }
+    if(i == 15) lcd.setCursor(0,1);
+  }
+}
 
 void printLetters(){
   for(int i = 0;i < 26;i++){
@@ -59,12 +64,13 @@ void setup() {
   pinMode(PIN_SEL, INPUT);
 }
 void loop() {
-  handleInput();
   if(screen){
+    handleInput();
     printLetters();
   }
   else{
-    //printWord();
+    printWord();
+
   }
 
   delay(1000);
