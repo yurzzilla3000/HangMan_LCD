b4c472d3fa78b462c05005a8f16e4c87f0a0c4f0
yurzzilla <bu277188@gmail.com>
2026-03-11 10:59:00 +0200

    First

--- HangMan_LCD.ino
+++ HangMan_LCD.ino
@@ -2,25 +2,53 @@
 #define PIN_NEXT 4
 #define PIN_PREV 2
 #define PIN_SEL 3
+
+
+
 bool screen = 0;
+byte lives = 9;
 byte selected = 14;
 LiquidCrystal_I2C lcd(0x27,16,2);
 char letters[26] = "abcdefghijklmnopqrstuvwxyz";
 const char* words[] = {"hello\0","cool\0", "frog\0", "idk\0"};
-const char* PlayWord = words[random(0,3)];
-char letters_guessed[3] = "oca";
+const char* PlayWord = "apple";
+char letters_guessed[26] = "                          ";
+byte count_guessed = 0;
 void printWord(){
   lcd.clear();
   for(int i = 0; i < 31; i++){
+    bool guessed = false;
+    if(i == 16) lcd.setCursor(0,1);
     if(PlayWord[i] == '\0'){
       break;
     }
-    for(int j = 0; j < 26; j++){
-      if(letters_guessed[j] == PlayWord[i]) lcd.print(PlayWord[i]);
+    for(int j = 0; j < count_guessed; j++){
+      if(letters_guessed[j] == PlayWord[i]){
+        guessed = true;
+        break;
+      }
     }
-    if(i == 15) lcd.setCursor(0,1);
+    if(guessed) lcd.print(PlayWord[i]);
+    else lcd.print("_");
+    
   }
 }
+
+void guess_letter(int id){
+  bool found = false;
+    for(int i = 0; i < 31; i++){
+      if(PlayWord[i] == id){
+        letters_guessed[count_guessed] = letters[id];
+        count_guessed += 1;
+        Serial.println(letters_guessed);
+        found = true;
+      }
+      if(PlayWord[i] == '\0') break;
+      
+    }
+   if(!found) lives--; 
+  }
+  
 
 void printLetters(){
   for(int i = 0;i < 26;i++){
@@ -55,23 +83,30 @@ void handleInput(){
     }
 }
 void setup() {
-  randomSeed(analogRead(A0));
+  randomSeed(analogRead(A7));
   lcd.init();
   lcd.backlight();
   lcd.blink();
   pinMode(PIN_NEXT, INPUT);
   pinMode(PIN_PREV, INPUT);
   pinMode(PIN_SEL, INPUT);
+  Serial.begin(9600);
 }
 void loop() {
-  if(screen){
-    handleInput();
-    printLetters();
+  if(lives){    
+    if(screen){
+      handleInput();
+      printLetters();
+    }
+    else{
+      printWord();
+      if(Serial.available() > 0){
+        guess_letter((int)Serial.read());
+      }
+    }
+  }else{
+    lcd.clear();
+    lcd.print("Game Over!");
   }
-  else{
-    printWord();
-
-  }
-
-  delay(1000);
+  delay(50);
 }
