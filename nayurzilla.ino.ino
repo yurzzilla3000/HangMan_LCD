852f022984143b8b630fd7e53ec6e7a04ca779b6
HackerCatcho <boyannikolov2011pro@gmail.com>
2026-03-13 18:33:34 +0200

    fixed winning when you're not supposed to

--- nayurzilla.ino.ino
+++ nayurzilla.ino.ino
@@ -7,23 +7,26 @@ extern bool is_full_guessed;
 void printWord(){
   lcd.clear();
   lcd.noCursor();
+
+  is_full_guessed = true;
+
   for(int i = 0; i < 31; i++){
     bool guessed = false;
+
     if(i == 16) lcd.setCursor(0,1);
-    if(PlayWord[i] == '\0'){
-      break;
-    }
+    if(PlayWord[i] == '\0') break;
+
     for(int j = 0; j < count_guessed; j++){
       if(letters_guessed[j] == PlayWord[i]){
         guessed = true;
         break;
       }
     }
-    if(guessed) {
+
+    if(guessed){
       lcd.print(PlayWord[i]);
-      is_full_guessed = true;
-      }
-    else{ 
+    }
+    else{
       lcd.print("_");
       is_full_guessed = false;
     }
@@ -31,18 +34,24 @@ void printWord(){
 }
 void guess_letter(char id){
   bool found = false;
-  char letter_s = id+97;
-    for(int i = 0; i < 31; i++){
-      if(PlayWord[i] == letter_s){
-        letters_guessed[count_guessed] = letters[id];
-        count_guessed += 1;
-        Serial.println(letters_guessed);
-        found = true;
-      }
-      if(PlayWord[i] == '\0') break;
-      
+  char letter_s = id + 97;
+
+  for(int i = 0; i < 31; i++){
+    if(PlayWord[i] == '\0') break;
+
+    if(PlayWord[i] == letter_s){
+      found = true;
     }
-  letters[id] = " ";
-  if(!found) lives--; 
-  screen = 0;
   }
+
+  if(found){
+    letters_guessed[count_guessed] = letter_s;
+    count_guessed++;
+  } 
+  else{
+    lives--;
+  }
+
+  letters[id] = " ";
+  screen = 0;
+}
